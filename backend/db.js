5e7ac072809852098481f7991a6a28eb97567913
Justin Looi <justinlooijw@gmail.com>
2023-08-23 19:50:04 +0800

    feature: changed to remote DB instead of localDB

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 const dbName = "keepMERN";
 const localDB = `mongodb://localhost:27017/${dbName}`;
+const password = "Test123";
+const remoteDB = `mongodb+srv://admin-justin:${password}@cluster0.ekr2imi.mongodb.net/${dbName}`;
 
 const connectDB = () => {
   try {
-    mongoose.connect(localDB, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+    mongoose.connect(remoteDB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log("MongoDB Connected...");
   } catch (err) {
@@ -16,5 +18,3 @@ const connectDB = () => {
 };
 
 module.exports = connectDB;
-
-  
