80df2f333a22af1af634be456db817abe3044f11
Just111n <105800434+Just111n@users.noreply.github.com>
2023-08-24 12:53:12 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://keep-mern.vercel.app/"],
+    methods: ["POST","GET","DELETE"],
+    credentials: true
+  }
+));
 
 app.use("/api/note", noteRouter);
 
